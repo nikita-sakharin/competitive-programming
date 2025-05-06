ccaa6c78d1d54d584c909d73e5676d3d7d48067d
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2025-05-06 22:49:19 +0100

    leetcode: 129

--- leetcode/129.cpp
+++ leetcode/129.cpp
@@ -18,12 +18,13 @@ private:
 
     static constexpr int initLeaf(
         const TreeNode *treeNode,
-        Stack<const TreeNode *> &lifo,
+        Stack<pair<const TreeNode *, int>> &lifo,
         int number
     ) noexcept {
         do {
-            lifo.push(treeNode);
             number = number * radix + treeNode->val;
+            if (treeNode->left && treeNode->right)
+                lifo.emplace(treeNode, number);
             if (treeNode->left)
                 treeNode = treeNode->left;
             else
@@ -33,39 +34,25 @@ private:
         return number;
     }
 
-    static constexpr bool hasRightSibling(
-        const TreeNode * const treeNode,
-        const TreeNode * const parent
-    ) noexcept {
-        return treeNode == parent->left && parent->right;
-    }
-
     static constexpr int nextLeaf(
-        Stack<const TreeNode *> &lifo,
-        int number
+        Stack<pair<const TreeNode *, int>> &lifo
     ) noexcept {
-        const TreeNode *treeNode{};
-        do {
-            treeNode = lifo.top();
-            lifo.pop();
-            number /= radix;
-        } while (!empty(lifo) && !hasRightSibling(treeNode, lifo.top()));
-
-        if (!empty(lifo))
-            number = initLeaf(lifo.top()->right, lifo, number);
+        auto [treeNode, number] = lifo.top();
+        lifo.pop();
+        number = initLeaf(treeNode->right, lifo, number);
 
         return number;
     }
 
 public:
     constexpr int sumNumbers(const TreeNode * const root) const noexcept {
-        Stack<const TreeNode *> lifo{};
+        Stack<pair<const TreeNode *, int>> lifo{};
         auto number{initLeaf(root, lifo, 0)}, result{number};
 
-        do {
-            number = nextLeaf(lifo, number);
+        while (!empty(lifo)) {
+            number = nextLeaf(lifo);
             result += number;
-        } while (!empty(lifo));
+        }
 
         return result;
     }
