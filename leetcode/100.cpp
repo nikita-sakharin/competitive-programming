1a6fbf5e507ae64994f806a94f1a1a5ebe184a45
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2025-05-09 11:20:21 +0100

    leetcode: 100

--- leetcode/100.cpp
+++ leetcode/100.cpp
@@ -19,20 +19,21 @@ public:
         const TreeNode * const first,
         const TreeNode * const second
     ) const noexcept {
+        if (!first && !second)
+            return true;
+
         Stack<pair<const TreeNode *, const TreeNode *>> lifo{};
         lifo.emplace(first, second);
 
         do {
             const auto [left, right]{lifo.top()};
             lifo.pop();
-            if (bool(left) != bool(right))
+            if (bool(left) != bool(right) || left->val != right->val)
                 return false;
-            if (left) {
-                if (left->val != right->val)
-                    return false;
+            if (left->left || right->left)
                 lifo.emplace(left->left, right->left);
+            if (left->right || right->right)
                 lifo.emplace(left->right, right->right);
-            }
         } while (!empty(lifo));
 
         return true;
