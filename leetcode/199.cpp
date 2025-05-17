5288ff96e92e17df31ad9b705068d3b64b934618
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2025-05-18 00:52:52 +0100

    leetcode: 199

--- leetcode/199.cpp
+++ leetcode/199.cpp
@@ -10,10 +10,12 @@
  * };
  */
 class Solution final {
-public:
-    constexpr vector<int> rightSideView(
+    template<class T>
+    using Stack = stack<T, vector<T>>;
+
+    static constexpr vector<int> rightSideViewBFS(
         const TreeNode * const root
-    ) const noexcept {
+    ) noexcept {
         vector<int> result{};
         if (!root)
             return result;
@@ -36,5 +38,40 @@ public:
         } while (!empty(fifo));
 
         return result;
+    }
+
+    static constexpr vector<int> rightSideViewDFS(
+        const TreeNode * const root
+    ) noexcept {
+        using Task = pair<const TreeNode *, size_t>;
+
+        vector<int> result{};
+        if (!root)
+            return result;
+
+        Stack<Task> lifo{};
+        lifo.emplace(root, 0);
+
+        do {
+            const auto [treeNode, depth]{lifo.top()};
+            lifo.pop();
+            if (depth >= result.size())
+                result.push_back(treeNode->val);
+            else
+                result[depth] = treeNode->val;
+            if (treeNode->right)
+                lifo.emplace(treeNode->right, depth + 1);
+            if (treeNode->left)
+                lifo.emplace(treeNode->left, depth + 1);
+        } while (!empty(lifo));
+
+        return result;
+    }
+
+public:
+    constexpr vector<int> rightSideView(
+        const TreeNode * const root
+    ) const noexcept {
+        return rightSideViewDFS(root);
     }
 };
