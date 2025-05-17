b86614a0e280eb49f5cb41330b931a45e2d6bf6c
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2025-05-18 00:05:48 +0100

    leetcode: 100 101

--- leetcode/100.cpp
+++ leetcode/100.cpp
@@ -11,30 +11,79 @@
  */
 class Solution final {
 private:
+    template<class Container>
+    using Value = Container::value_type;
+
+    template<class Container>
+    struct Front final {
+    public:
+        constexpr const Value<Container> &operator()(
+            const Container &container
+        ) const noexcept {
+            return container.front();
+        }
+    };
+
+    template<class Container>
+    struct Top final {
+    public:
+        constexpr const Value<Container> &operator()(
+            const Container &container
+        ) const noexcept {
+            return container.top();
+        }
+    };
+
+    template<template<class> class Container, template<class> class Accessor>
+    static constexpr bool isSameTree(
+        const TreeNode * const first,
+        const TreeNode * const second
+    ) noexcept {
+        using Task = pair<const TreeNode *, const TreeNode *>;
+
+        if (!first && !second)
+            return true;
+
+        Container<Task> container{};
+        const Accessor<Container<Task>> accessor{};
+        container.emplace(first, second);
+
+        do {
+            const auto [left, right]{accessor(container)};
+            container.pop();
+            if (bool(left) != bool(right) || left->val != right->val)
+                return false;
+            if (left->left || right->left)
+                container.emplace(left->left, right->left);
+            if (left->right || right->right)
+                container.emplace(left->right, right->right);
+        } while (!empty(container));
+
+        return true;
+    }
+
+    static constexpr bool isSameTreeBreadthFirstSearch(
+        const TreeNode * const first,
+        const TreeNode * const second
+    ) noexcept {
+        return isSameTree<queue, Front>(first, second);
+    }
+
     template<class T>
     using Stack = stack<T, vector<T>>;
+
+    static constexpr bool isSameTreeDepthFirstSearch(
+        const TreeNode * const first,
+        const TreeNode * const second
+    ) noexcept {
+        return isSameTree<Stack, Top>(first, second);
+    }
 
 public:
     constexpr bool isSameTree(
         const TreeNode * const first,
         const TreeNode * const second
     ) const noexcept {
-        Stack<pair<const TreeNode *, const TreeNode *>> lifo{};
-        lifo.emplace(first, second);
-
-        do {
-            const auto [left, right]{lifo.top()};
-            lifo.pop();
-            if (bool(left) != bool(right))
-                return false;
-            if (left) {
-                if (left->val != right->val)
-                    return false;
-                lifo.emplace(left->left, right->left);
-                lifo.emplace(left->right, right->right);
-            }
-        } while (!empty(lifo));
-
-        return true;
+        return isSameTreeDepthFirstSearch(first, second);
     }
 };
--- leetcode/101.cpp
+++ leetcode/101.cpp
@@ -11,27 +11,71 @@
  */
 class Solution final {
 private:
+    template<class Container>
+    using Value = Container::value_type;
+
+    template<class Container>
+    struct Front final {
+    public:
+        constexpr const Value<Container> &operator()(
+            const Container &container
+        ) const noexcept {
+            return container.front();
+        }
+    };
+
+    template<class Container>
+    struct Top final {
+    public:
+        constexpr const Value<Container> &operator()(
+            const Container &container
+        ) const noexcept {
+            return container.top();
+        }
+    };
+
+    template<template<class> class Container, template<class> class Accessor>
+    static constexpr bool isSymmetric(const TreeNode * const root) noexcept {
+        using Task = pair<const TreeNode *, const TreeNode *>;
+
+        if (!root->left && !root->right)
+            return true;
+
+        Container<Task> container{};
+        const Accessor<Container<Task>> accessor{};
+        container.emplace(root->left, root->right);
+
+        do {
+            const auto [left, right]{accessor(container)};
+            container.pop();
+            if (bool(left) != bool(right) || left->val != right->val)
+                return false;
+            if (left->left || right->right)
+                container.emplace(left->left, right->right);
+            if (left->right || right->left)
+                container.emplace(left->right, right->left);
+        } while (!empty(container));
+
+        return true;
+    }
+
+    static constexpr bool isSymmetricBreadthFirstSearch(
+        const TreeNode * const root
+    ) noexcept {
+        return isSymmetric<queue, Front>(root);
+    }
+
     template<class T>
     using Stack = stack<T, vector<T>>;
 
+    static constexpr bool isSymmetricDepthFirstSearch(
+        const TreeNode * const root
+    ) noexcept {
+        return isSymmetric<Stack, Top>(root);
+    }
+
 public:
     constexpr bool isSymmetric(const TreeNode * const root) const noexcept {
-        Stack<pair<const TreeNode *, const TreeNode *>> lifo{};
-        lifo.emplace(root->left, root->right);
-
-        do {
-            const auto [left, right]{lifo.top()};
-            lifo.pop();
-            if (bool(left) != bool(right))
-                return false;
-            if (left) {
-                if (left->val != right->val)
-                    return false;
-                lifo.emplace(left->left, right->right);
-                lifo.emplace(left->right, right->left);
-            }
-        } while (!empty(lifo));
-
-        return true;
+        return isSymmetricDepthFirstSearch(root);
     }
 };
