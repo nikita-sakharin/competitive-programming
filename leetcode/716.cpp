1e1183744136e01ed1f332f38acaa8ace66df53e
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2025-06-01 23:56:32 +0100

    leetcode: 716

--- leetcode/716.cpp
+++ leetcode/716.cpp
@@ -6,22 +6,22 @@ private:
     using ConstIterator = list<int>::const_iterator;
 
     list<int> values{};
-    map<int, Stack<ConstIterator>> index{};
+    map<int, Stack<ConstIterator>> dict{};
 
 public:
     constexpr void push(const int value) noexcept {
         values.push_back(value);
-        index[value].push(--cend(values));
+        dict[value].push(--cend(values));
     }
 
     constexpr int pop() noexcept {
         const auto value{values.back()};
-        const auto iter{index.find(value)};
+        const auto iter{dict.find(value)};
         auto &iterStack{iter->second};
         values.pop_back();
         iterStack.pop();
         if (empty(iterStack))
-            index.erase(iter);
+            dict.erase(iter);
 
         return value;
     }
@@ -31,17 +31,17 @@ public:
     }
 
     constexpr int peekMax() const noexcept {
-        return (--cend(index))->first;
+        return (--cend(dict))->first;
     }
 
     constexpr int popMax() noexcept {
-        const auto iter{--end(index)};
+        const auto iter{--end(dict)};
         const auto value{iter->first};
         auto &iterStack{iter->second};
         values.erase(iterStack.top());
         iterStack.pop();
         if (empty(iterStack))
-            index.erase(iter);
+            dict.erase(iter);
 
         return value;
     }
