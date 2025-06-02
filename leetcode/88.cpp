49309ccce8e426927644176feb8fefbcd639c99d
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2025-06-02 10:20:38 +0100

    leetcode: 88

--- leetcode/88.cpp
+++ leetcode/88.cpp
@@ -1,17 +1,17 @@
 class Solution final {
 public:
     constexpr void merge(
-        vector<int> &nums1, int m,
-        vector<int> &nums2, int n
+        vector<int> &values1, ptrdiff_t size1,
+        const vector<int> &values2, ptrdiff_t size2
     ) const noexcept {
-        --m, --n;
-        while (m >= 0 || n >= 0)
-            if (m < 0 || (n >= 0 && nums1[m] < nums2[n])) {
-                nums1[m + n + 1] = nums2[n];
-                --n;
+        --size1, --size2;
+        while (size1 >= 0 || size2 >= 0)
+            if (size1 < 0 || (size2 >= 0 && values1[size1] < values2[size2])) {
+                values1[size1 + size2 + 1] = values2[size2];
+                --size2;
             } else {
-                nums1[m + n + 1] = nums1[m];
-                --m;
+                values1[size1 + size2 + 1] = values1[size1];
+                --size1;
             }
     }
 };
